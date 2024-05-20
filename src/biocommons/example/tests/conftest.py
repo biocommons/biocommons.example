776c09ac76fae24591b9cc90dc47787763c1fa8d
James Stevenson <james.stevenson@nationwidechildrens.org>
2024-05-20 16:33:18 -0400

    make CI pass

--- src/biocommons/example/tests/conftest.py
+++ src/biocommons/example/tests/conftest.py
@@ -4,7 +4,7 @@ import pytest
 import yaml
 
 
-@pytest.fixture
+@pytest.fixture()
 def all_quotes():
     quotes_stream = importlib.resources.files("example").joinpath("quotes.yaml").read_text()
     return yaml.load(quotes_stream, Loader=yaml.SafeLoader)["quotes"]
