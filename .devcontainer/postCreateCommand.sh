7a78988fe41a1f9b2ec69457c29796aefaf2f8d2
Reece Hart <reece@biocommons.org>
2025-08-30 12:11:14 -0700

    parked the devcontainer config, which is still broken

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,10 +1,5 @@
 #! /usr/bin/env bash
 
-# Install uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
-
-# Install Dependencies
-uv sync
-
-# Install pre-commit hooks
-uv run pre-commit install --install-hooks
+uv sync --link-mode=copy
+uv run --link-mode=copy pre-commit install --install-hooks
